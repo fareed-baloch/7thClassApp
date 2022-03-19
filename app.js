7f4b2dcde16c63e12724afb59dd09a87bdb89390
Fareed <ucdon1249@gmail.com>
2022-03-19 10:25:19 +0500

    added multer

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require("express")
 const app = express()
 const port = 3000
 const Posts = require('./models/posts');
-
+const multer = require('multer');
+const path = require('path');
 //configs
 //set the view engine to ejs
 
@@ -15,26 +16,126 @@ app.use(
 
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
+
+//config for multer
+const storage = multer.diskStorage({
+  destination: 'public/uploads/',
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+  }
+});
+
+const upload = multer({
+  storage: storage,
+  fileFilter: function (req, file, cb) {
+    checkFileType(file, cb);
+  }
+}).single('filetoupload');
+
+function checkFileType(file, cb) {
+  const filetypes = /png|jpg|jpeg/;
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  const mimetype = filetypes.test(file.mimetype);
+  if (mimetype && extname) {
+    return cb(null, true);
+  } else {
+    cb('ERROR : Images only !');
+  }
+}
+
+
 //end configs
 
 
-  app.get('/', (req, res) => {
-    //$result = select * from post; 
-      res.render('blank');
-    })
-     app.get('/posts', async (req, res,next) => {
-     try {
-            res.json(await Posts.getMultiple());
-          } catch (err) {
-            console.error(`Error while getting Posts `, err.message);
-            next(err);
-          }
-      
-    })
+app.get('/', (req, res) => {
+  //$result = select * from post; 
+  res.render('blank');
+})
+app.get('/posts', async (req, res, next) => {
+  try {
+    let row = await Posts.getMultiple()
+    res.render('Posts/index',{data:row.data});
+  // res.json(row.data);
+  } catch (err) {
+    console.error(`Error while getting Posts `, err.message);
+    next(err);
+  }
 
-    
+})
+
+app.post('/post/store',upload, async function (req, res, next) {
+  try {
+    let Postvalues = {
+      title: req.body.title,
+      descripton: req.body.descripton,
+      image:req.file.filename
+    };
+
+    res.json(await Posts.create(Postvalues));
+  } catch (err) {
+    console.error(`Error while creating Post`, err.message);
+    next(err);
+  }
+});
+
+app.get('/post/create', (req, res) => {
+  res.render('Posts/create');
+})
 
 
-  app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+app.get('/post/delete/:id', async function (req, res, next) {
+  try {
+   // console.log(req.params.id);
+   res.json(await Posts.remove(req.params.id))
+  } catch (err) {
+    console.error(`Error while updating Post`, err.message);
+    next(err);
+  }
+});
+
+app.get('/post/edit/:id', async function (req, res, next) {
+  try {
+    let row = await Posts.getSingle(req.params.id)
+    // res.json(row.data[0]);
+   res.render('Posts/update',{data:row.data[0]});
+  } catch (err) {
+    console.error(`Error while updating Post`, err.message);
+    next(err);
+  }
+});
+
+app.post('/post/update/:id', async function (req, res, next) {
+  try {
+    let PostUpdateValues = {
+      title:req.body.title,
+      descripton:req.body.descripton
+    }
+
+    res.json(await Posts.update(req.params.id, PostUpdateValues));
+
+  } catch (err) {
+    console.error(`Error while updating Post`, err.message);
+    next(err);
+  }
+});
+
+app.post('/post/update-image/:id',upload, async function (req, res, next) {
+  try {
+    let PostUpdateValues = {
+     image:req.file.filename,
+    }
+
+    res.json(await Posts.update_image(req.params.id, PostUpdateValues));
+
+  } catch (err) {
+    console.error(`Error while updating Post`, err.message);
+    next(err);
+  }
+});
+
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
 })
--- models/posts.js
+++ models/posts.js
@@ -1,22 +1,104 @@
 const db = require('../config/db');
 const helper = require('../config/helper');
-//const config = require('../config/database');
 
-async function getMultiple(page = 1){
- // const offset = helper.getOffset(page, config.listPerPage);
- //`SELECT * FROM posts LIMIT ${offset},${config.listPerPage}`
- const rows = await db.query(
+
+async function getMultiple(page = 1) {
+  // const offset = helper.getOffset(page, config.listPerPage);
+  //`SELECT * FROM posts LIMIT ${offset},${config.listPerPage}`
+  const rows = await db.query(
     `SELECT * FROM posts`
   );
   const data = helper.emptyOrRows(rows);
   //const meta = {page};
   return {
     data
-   // meta
+    // meta
   }
 }
 
+async function getSingle(id) {
+  // const offset = helper.getOffset(page, config.listPerPage);
+  //`SELECT * FROM posts LIMIT ${offset},${config.listPerPage}`
+  const rows = await db.query(
+    `SELECT * FROM posts where id = ${id}`
+  );
+  const data = helper.emptyOrRows(rows);
+  //const meta = {page};
+  return {
+    data
+  }
+}
+
+async function create(post) {
+  const result = await db.query(
+    `INSERT INTO posts(id, title, des, image) VALUES (default,'${post.title}','${post.descripton}','${post.image}')`
+  );
+
+  let message = 'Error in creating Posts';
+
+  if (result.affectedRows) {
+    message = 'Post created successfully';
+  }
+
+  return {
+    message
+  };
+}
+
+async function update(id, Posts) {
+  const result = await db.query(
+    `UPDATE posts SET title='${Posts.title}' , des='${Posts.descripton}' WHERE id = ${id}`
+  );
+
+  let message = 'Error in updating Post';
+
+  if (result.affectedRows) {
+    message = 'Post updated successfully';
+  }
+
+  return {
+    message
+  };
+}
+
+
+async function update_image(id, Posts) {
+  const result = await db.query(
+    `UPDATE posts SET image='${Posts.image}' WHERE id = ${id}`
+  );
+
+  let message = 'Error in updating Post';
+
+  if (result.affectedRows) {
+    message = 'Post updated successfully';
+  }
+
+  return {
+    message
+  };
+}
+async function remove(id) {
+  const result = await db.query(
+    `DELETE FROM posts WHERE id=${id}`
+  );
+
+  let message = 'Error in deleting Posts';
+
+  if (result.affectedRows) {
+    message = 'Posts deleted successfully';
+  }
+
+  return {
+    message
+  };
+}
 
 module.exports = {
-  getMultiple
+  getMultiple,
+  create,
+  remove,
+  getSingle,
+  update,
+  update_image,
+
 }
